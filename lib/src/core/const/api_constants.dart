835c117f3adc7a799bb75762f866bfc79a41b13d
Vikas Verma <vermasingh2002@gmail.com>
2026-09-02 12:13:18 +0530

    Update base URL in API constants for local environment
    - Update `baseUrl` IP address in `ApiConstants` to point to the new backend server host.

--- lib/src/core/const/api_constants.dart
+++ lib/src/core/const/api_constants.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/foundation.dart';
 
 @immutable
-abstract final class ApiConstants{
+abstract final class ApiConstants {
   const ApiConstants._();
 
-  static const String baseUrl = "http://192.168.1.8:8083";
+  static const String baseUrl = "http://192.168.1.21:8083";
   static const String tokenApiEndpoint = "$baseUrl/api/admin/get-token";
   static const String roleApiEndpoint = "$baseUrl/api/admin/role-type";
   static const String loginApiEndpoint = "$baseUrl/api/admin/login";
