39b4472637bb866be6d699ffe7cfc7e7adb5d21e
Vikas Verma <109075366+Vikas100702@users.noreply.github.com>
2026-09-04 17:40:08 +0530

    Update API base URL and introduce product category endpoint
    - Update `baseUrl` in `ApiConstants` to point to the remote server environment.
    - Add `viewCategoryApiEndpoint` to support fetching product categories.

--- lib/src/core/const/api_constants.dart
+++ lib/src/core/const/api_constants.dart
@@ -4,7 +4,7 @@ import 'package:flutter/foundation.dart';
 abstract final class ApiConstants {
   const ApiConstants._();
 
-  static const String baseUrl = "http://192.168.1.21:8083";
+  static const String baseUrl = "https://abctest.animationmedia.org";
   static const String tokenApiEndpoint = "$baseUrl/api/admin/get-token";
   static const String roleApiEndpoint = "$baseUrl/api/admin/role-type";
   static const String loginApiEndpoint = "$baseUrl/api/admin/login";
@@ -12,4 +12,5 @@ abstract final class ApiConstants {
   static const String getPincodeApiEndpoint = "$baseUrl/api/firm/getPincode";
   static const String addPincodeApiEndpoint = "$baseUrl/api/firm/addPincode";
   static const String unmapPincodeApiEndpoint = "$baseUrl/api/firm/unmapAndAssign";
+  static const String viewCategoryApiEndpoint = "$baseUrl/api/product/viewCategory";
 }
