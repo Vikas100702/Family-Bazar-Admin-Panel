ff949c927fa224d28cb925505a5df92a08dcbced
Vikas Verma <109075366+Vikas100702@users.noreply.github.com>
2026-09-09 00:42:08 +0530

    Refactor CategoryBinding to remove StorageService dependency
    - Update `CategoryController` initialization to remove the unused `StorageService` dependency.
    - Remove the unnecessary `storage_services.dart` import to clean up the binding configuration.

--- lib/src/modules/dashboard/modules/product/category/binding/category_binding.dart
+++ lib/src/modules/dashboard/modules/product/category/binding/category_binding.dart
@@ -1,5 +1,4 @@
 import 'package:family_bazar_admin_panel/src/core/network/api_client.dart';
-import 'package:family_bazar_admin_panel/src/core/utils/storage/storage_services.dart';
 import 'package:family_bazar_admin_panel/src/modules/dashboard/modules/product/category/controller/category_controller.dart';
 import 'package:family_bazar_admin_panel/src/modules/dashboard/modules/product/category/repository/category_repository.dart';
 import 'package:get/get.dart';
@@ -8,8 +7,6 @@ class CategoryBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<CategoryRepository>(() => CategoryRepository(apiClient: Get.find<ApiClient>()));
-    Get.lazyPut<CategoryController>(
-      () => CategoryController(categoryRepository: Get.find<CategoryRepository>(), storageService: Get.find<StorageService>()),
-    );
+    Get.lazyPut<CategoryController>(() => CategoryController(categoryRepository: Get.find<CategoryRepository>()));
   }
 }
