33ca9905dd7c066506fc4e8a223a9bd1eded4e39
Vikas Verma <109075366+Vikas100702@users.noreply.github.com>
2026-09-09 00:52:40 +0530

    Integrate SubCategory and Item module bindings into Dashboard
    - Update `DashboardBinding` to include `SubCategoryBinding` and `ItemBinding` dependencies, enabling the initialization of sub-category and item management logic within the dashboard module.

--- lib/src/modules/dashboard/binding/dashboard_binding.dart
+++ lib/src/modules/dashboard/binding/dashboard_binding.dart
@@ -3,6 +3,8 @@ import 'package:family_bazar_admin_panel/src/modules/dashboard/modules/drawer/bi
 import 'package:family_bazar_admin_panel/src/modules/dashboard/modules/firm/binding/firm_setup_binding.dart';
 import 'package:family_bazar_admin_panel/src/modules/dashboard/modules/pincode_settings/binding/pincode_settings_binding.dart';
 import 'package:family_bazar_admin_panel/src/modules/dashboard/modules/product/category/binding/category_binding.dart';
+import 'package:family_bazar_admin_panel/src/modules/dashboard/modules/product/item/binding/item_binding.dart';
+import 'package:family_bazar_admin_panel/src/modules/dashboard/modules/product/sub_category/binding/sub_cat_binding.dart';
 import 'package:get/get.dart';
 
 class DashboardBinding extends Bindings {
@@ -18,5 +20,7 @@ class DashboardBinding extends Bindings {
     FirmBinding().dependencies();
     PincodeSettingsBinding().dependencies();
     CategoryBinding().dependencies();
+    SubCategoryBinding().dependencies();
+    ItemBinding().dependencies();
   }
 }
