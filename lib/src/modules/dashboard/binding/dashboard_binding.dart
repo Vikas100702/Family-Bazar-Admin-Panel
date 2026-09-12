50208442cae016b318a388eb27b8d1331b43de37
Vikas Verma <109075366+Vikas100702@users.noreply.github.com>
2026-09-12 22:40:59 +0530

    Integrate Dashboard Group module binding into Dashboard
    - Update `DashboardBinding` to include `DashboardGroupBinding` dependencies, enabling the initialization of dashboard group management logic within the product module.

--- lib/src/modules/dashboard/binding/dashboard_binding.dart
+++ lib/src/modules/dashboard/binding/dashboard_binding.dart
@@ -3,6 +3,7 @@ import 'package:family_bazar_admin_panel/src/modules/dashboard/modules/drawer/bi
 import 'package:family_bazar_admin_panel/src/modules/dashboard/modules/firm/binding/firm_setup_binding.dart';
 import 'package:family_bazar_admin_panel/src/modules/dashboard/modules/pincode_settings/binding/pincode_settings_binding.dart';
 import 'package:family_bazar_admin_panel/src/modules/dashboard/modules/product/category/binding/category_binding.dart';
+import 'package:family_bazar_admin_panel/src/modules/dashboard/modules/product/dashboard_group/binding/dashboard_group_binding.dart';
 import 'package:family_bazar_admin_panel/src/modules/dashboard/modules/product/item/binding/item_binding.dart';
 import 'package:family_bazar_admin_panel/src/modules/dashboard/modules/product/sub_category/binding/sub_cat_binding.dart';
 import 'package:get/get.dart';
@@ -22,5 +23,6 @@ class DashboardBinding extends Bindings {
     CategoryBinding().dependencies();
     SubCategoryBinding().dependencies();
     ItemBinding().dependencies();
+    DashboardGroupBinding().dependencies();
   }
 }
