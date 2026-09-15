678bfcb8d8e720f636d70e506977954bb9216bf4
Vikas Verma <109075366+Vikas100702@users.noreply.github.com>
2026-09-15 23:44:29 +0530

    Organize API constants and add dashboard group update endpoint
    - Categorize existing API endpoints using descriptive comments for Auth, Image, Firm, Pincode, Category, Item, and Dashboard groups to improve code organization.
    - Add `updateDashboardGroupApiEndpoint` to the `ApiConstants` class to support dashboard group update functionality.
    - Improve the readability and structure of the `api_constants.dart` file by grouping related endpoints.

--- lib/src/core/const/api_constants.dart
+++ lib/src/core/const/api_constants.dart
@@ -15,23 +15,41 @@ abstract final class ApiConstants {
     return '$baseUrl$formattedPath';
   }
 
+  /// AUTH APIs
   static const String tokenApiEndpoint = "$baseUrl/api/admin/get-token";
   static const String roleApiEndpoint = "$baseUrl/api/admin/role-type";
   static const String loginApiEndpoint = "$baseUrl/api/admin/login";
+
+  /// Image API
   static const String uploadImgApiEndpoint = "$baseUrl/api/product/uploadImage";
+
+  /// FIRM API
   static const String viewFirmApiEndpoint = "$baseUrl/api/firm/viewFirm";
+
+  /// PIN CODE APIs
   static const String getPincodeApiEndpoint = "$baseUrl/api/firm/getPincode";
   static const String insertPincodeApiEndpoint = "$baseUrl/api/firm/addPincode";
   static const String unmapPincodeApiEndpoint = "$baseUrl/api/firm/unmapAndAssign";
+
+  /// CATEGORY APIs
   static const String viewCategoryApiEndpoint = "$baseUrl/api/product/viewCategory";
   static const String insertCategoryDetailsApiEndpoint = "$baseUrl/api/product/addCategory";
+
+  /// SUBCATEGORY APIs
   static const String viewSubCategoryApiEndpoint = "$baseUrl/api/product/viewSubcategory";
   static const String insertSubCatDetailsApiEndpoint = "$baseUrl/api/product/addSubCategory";
+
+  /// ITEM APIs
   static const String viewItemsApiEndpoint = "$baseUrl/api/product/viewItem";
   static const String insertItemDetailsApiEndpoint = "$baseUrl/api/product/addItem";
+
+  /// DASHBOARD GROUP APIs
   static const String viewDashboardGroupApiEndpoint = "$baseUrl/api/product/viewGroup";
   static const String addDashboardGroupApiEndpoint = "$baseUrl/api/product/addGroup";
   static const String deleteDashboardGroupApiEndpoint = "$baseUrl/api/product/deleteGroup";
+  static const String updateDashboardGroupApiEndpoint = "$baseUrl/api/product/updateGroup";
+
+  /// DASHBOARD GROUP ITEM APIs
   static const String viewGroupItemsApiEndpoint = "$baseUrl/api/product/viewGroupItems";
   static const String addGroupItemsApiEndpoint = "$baseUrl/api/product/addGroupItems";
   static const String deleteGroupItemsApiEndpoint = "$baseUrl/api/product/deleteGroupItem";
